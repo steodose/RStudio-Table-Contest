2e81f6beba7b5fd259940b4b875d97cde2edf301
Stephan Teodosescu <32054537+steodose@users.noreply.github.com>
2021-10-25 20:58:04 -0700

    Added weekly change column

--- RStudio Table Contest 2021.R
+++ RStudio Table Contest 2021.R
@@ -5,9 +5,6 @@
 library(tidyverse)
 library(worldfootballR) #installed using "remotes::install_github("JaseZiv/worldfootballR")"
 library(magick)
-library(cowplot)
-library(rvest)
-library(polite)
 library(gt) #for 538-themed tables
 library(gtExtras) #for add-ons working with {gt} package
 library(webshot) #saving high quality images of gt tables
@@ -29,21 +26,12 @@ matchweek <- 9 # Specify how many full matchweeks have been played
 last_week <- matchweek-1
 
 
-##### Set up themes for plots and tables #####
-
-# Custom ggplot theme (inspired by Owen Phillips at the F5 substack blog)
-theme_custom <- function () { 
-    theme_minimal(base_size=11, base_family="Chivo") %+replace% 
-        theme(
-            panel.grid.minor = element_blank(),
-            plot.background = element_rect(fill = 'floralwhite', color = "floralwhite")
-        )
-}
+##### Set up themes for table #####
 
 # Define color palette to use in tables
 my_color_pal <- c("#ffffff", "#f2fbd2", "#c9ecb4", "#93d3ab", "#35b0ab")
 
-# Create 538 GT table theme from Thomas Mock's blog.Comes from this post: https://themockup.blog/posts/2020-09-26-functions-and-themes-for-gt-tables/?panelset3=theme-code2&panelset4=theme-code3 
+# Create 538 GT table theme from Thomas Mock's blog. Idea comes from this post: https://themockup.blog/posts/2020-09-26-functions-and-themes-for-gt-tables/?panelset3=theme-code2&panelset4=theme-code3 
 gt_theme_538 <- function(data,...) {
     data %>%
         # Add team logos w/ web_image
@@ -101,7 +89,7 @@ gt_theme_538 <- function(data,...) {
 
 ##### Data Wrangling #####
 
-## 1. 2021-22 league table highlighting Brentford
+## Tidy dataframe for input into table
 
 home <- EPL_2022 %>% 
     group_by(Home) %>%
@@ -183,11 +171,54 @@ EPL_2022_table3 <- EPL_2022_table2 %>%
     summarise(list_data = list(list_data)) %>% 
     ungroup()
 
+# Determine prior week rankings
+home_pw <- EPL_2022 %>% 
+    group_by(Home) %>%
+    filter(Wk <= last_week) %>% 
+    summarize(
+        MP = n(),
+        Pts = sum((HomeGoals > AwayGoals) * 3 + (HomeGoals == AwayGoals) * 1)
+        ) %>% 
+    ungroup()
 
-# Make table
-EPL_2022_table3 %>%
+away_pw <- EPL_2022 %>% 
+    group_by(Away) %>%
+    filter(Wk <= last_week) %>% 
+    summarize(
+        MP = n(),
+        Pts = sum((AwayGoals > HomeGoals) * 3 + (HomeGoals == AwayGoals) * 1)
+    ) %>% 
+    ungroup()
+
+joined_df_pw <- inner_join(home_pw, away_pw, by = c("Home" = "Away")) %>% 
+    group_by(Home) %>% 
+    mutate(
+        MP = sum(MP.x, MP.y),
+        Points = sum(Pts.x, Pts.y)) %>%
+    rename("Squad" = "Home") %>%
+    arrange(desc(Points)) %>% 
+    ungroup() %>% 
+    mutate(Rank = row_number()) %>% 
+    relocate(Rank) %>% 
+    rename("pw_rank" = "Rank") %>% 
+    select(Squad, pw_rank)
+
+# join together
+EPL_2022_table4 <- EPL_2022_table3 %>% 
+   right_join(joined_df_pw) %>% 
+    mutate(`1-Wk Change` = pw_rank - Rank) %>% 
+    select(-pw_rank) %>% 
+    select(Rank, `1-Wk Change`, url_logo_espn:list_data)
+
+
+
+##### Make Table Viz #####
+
+## Make league table highlighting Brentford
+
+EPL_2022_table4 %>%
     gt() %>%
-    data_color(columns = 4,
+    data_color(columns = 5,
                colors = scales::col_numeric(
                    palette = c("white", "#3fc1c9"),
                    domain = NULL)
@@ -198,7 +229,6 @@ EPL_2022_table3 %>%
             cell_fill(color = "#FFFAA0") #highlighting the Brentford row.
         ),
         locations = cells_body(rows = Squad=="Brentford")
-<<<<<<< HEAD
     ) %>%
     tab_style(
         style = cell_borders(sides = "bottom", color = "black", weight = px(1)),
@@ -207,8 +237,6 @@ EPL_2022_table3 %>%
     tab_style(
         style = cell_borders(sides = "bottom", color = "black", weight = px(1)),
         locations = cells_body(rows = 17)
-=======
->>>>>>> 418d1bd8d5540516634378d3cf4a075a700a17d2
     ) %>%
     tab_style(
         style = list(
@@ -248,13 +276,31 @@ EPL_2022_table3 %>%
     ) %>% 
     tab_spanner(
         label = "Via StatsBomb",
-        columns = c(9:11)
+        columns = c(10:12)
     ) %>% 
     cols_width(Squad ~ 200) %>% 
     cols_align(align = "left",
                columns = 1) %>%
+    cols_align(align = "center",
+               columns = 2) %>%
+    text_transform(
+        locations = cells_body(columns = `1-Wk Change`),
+        fn = function(x){
+            change <- as.integer(x)
+            choose_logo <-function(x){
+                if (x == 0){
+                    gt::html(fontawesome::fa("equals", fill = "#696969"))
+                } else if (x > 0){
+                    gt::html(glue::glue("<span style='color:#191970;text-indent:16px;font-face:bold;font-size:16px;'>{x}</span>"), fontawesome::fa("chevron-up", fill = "#1134A6"))
+                } else if (x < 0) {
+                    gt::html(glue::glue("<span style='color:#DA2A2A;font-face:bold;font-size:16px;'>{x}</span>"), fontawesome::fa("chevron-down", fill = "#DA2A2A"))
+                }
+            }
+            map(change, choose_logo)
+        }
+    ) %>%
     tab_header(title = md("**2021-22 Premier League Table**"),
-               subtitle = md(glue("**<span style = 'color:#e30613'>Brentford</span>** are well above the relegation zone in the club's first season in the Premier League. Their Expected Goals values indicate they may even be underperforming their true quality. Teams sorted based on points thru **Matchweek {matchweek}**."))) %>% 
+               subtitle = md(glue("**<span style = 'color:#e30613'>Brentford</span>** are well above the relegation zone in the club's first season in the Premier League. The Bees' Expected Goals values indicate they may even be underperforming their true quality. Teams sorted based on points thru **Matchweek {matchweek}**."))) %>% 
     tab_source_note(
         source_note = md("DATA: fbref.com via {worldfootballR}.<br>Table: @steodosescu (Between the Pipes) | Inspired by Tom Mock.")) %>% 
     gt_plt_bar_pct(column = `Points Percentage`, scaled = FALSE, fill = "navy", background = "gray") %>% 
